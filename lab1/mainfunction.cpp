ad56faff82d4dfecfa6269a9f2e0613507ce3069
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-10 00:04:54 +0300

    Create mainfunction.cpp

---
+++ lab1/mainfunction.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "..\Project35\Header.h"
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    FifteenPuzzle puzzle(4);
+
+    while (!puzzle.isSolved()) {
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                std::cout << puzzle[i * 4 + j] << " ";
+            }
+            std::cout << std::endl;
+        }
+
+        std::cout << "Введите число (1-15) для перемещения: ";
+        int value;
+        std::cin >> value;
+
+        if (!puzzle.move(value)) {
+            std::cout << "Недопустимый ход!" << std::endl;
+        }
+    }
+
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            std::cout << puzzle[i * 4 + j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "PUZZLE IS SOLVED!" << std::endl;
+
+    return 0;
+}
