0dfb1406790ad22cfd430b283426c6ac07881ea5
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:24:15 +0300

    Update PlumbingService.h

--- lab2/Repair company/PlumbingService/PlumbingService.h
+++ lab2/Repair company/PlumbingService/PlumbingService.h
@@ -1,1 +1,11 @@
+#pragma once
+#include "Service.h"
+#include<iostream>
+#include<string>
+using namespace std;
+class PlumbingService : public Service {
+public:
+	PlumbingService();
+	void inspectPlumbing();
+};
 
