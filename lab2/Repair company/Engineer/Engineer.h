b8cf4653119612c482eb79df89a15db8632bbb3a
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:23:02 +0300

    Update Engineer.h

--- lab2/Repair company/Engineer/Engineer.h
+++ lab2/Repair company/Engineer/Engineer.h
@@ -1,1 +1,16 @@
-
+#pragma once
+#include "Employee.h"
+#include<iostream>
+#include<string>
+using namespace std;
+class Engineer: public Employee {
+public:
+	Engineer(string Employee_name, int Employee_ID);
+	void perform_task() override;
+	void setCertificationLevel(int level);
+	void setSpecialization(string specialization);
+	void performQualityCheck();
+private:
+	int certificationLevel;
+	string specialization;
+};
