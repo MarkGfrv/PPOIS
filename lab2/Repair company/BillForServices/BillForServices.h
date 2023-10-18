9e7f3dd02bf20fa8f4edc934ddac6e1297fca9cf
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:19:47 +0300

    Update BillForServices.h

--- lab2/Repair company/BillForServices/BillForServices.h
+++ lab2/Repair company/BillForServices/BillForServices.h
@@ -1,1 +1,17 @@
-
+#pragma once
+#include "Customer.h"
+#include "Service.h"
+#include<iostream>
+#include<string>
+using namespace std;
+class BillForServices {
+public:
+	BillForServices(Customer& customer, Service& service, double TotalCost);
+	void generateBill();
+	void setPaymentStatus(string status);
+private:
+	Customer& customer;
+	Service& service;
+	double TotalCost;
+	string paymentStatus;
+};
