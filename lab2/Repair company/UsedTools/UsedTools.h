0785bfd24ac903fc3d96b55737640cdf39372c52
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:26:07 +0300

    Update UsedTools.h

--- lab2/Repair company/UsedTools/UsedTools.h
+++ lab2/Repair company/UsedTools/UsedTools.h
@@ -1,1 +1,14 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+class UsedTools {
+public:
+	UsedTools(string nameofUsedTool);
+	void useToolforService();
+	void setCondition(string condition);
+private:
+	string nameofUsedTool;
+	string toolCondition;
+};
 
