f7bdbb69ca55d16414257a505d3b2bfe3c2809ac
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:24:48 +0300

    Update RequestedService.h

--- lab2/Repair company/RequestedService/RequestedService.h
+++ lab2/Repair company/RequestedService/RequestedService.h
@@ -1,1 +1,22 @@
+#pragma once
+#include "Customer.h"
+#include "Service.h"
+#include "Employee.h"
+#include<iostream>
+#include<string>
+using namespace std;
+class RequestedService {
+public:
+	RequestedService(Customer& customer, Service& service, Employee& workingEmployee);
+	void CompleteOrder();
+	void setAppointmentDate(string date);
+	void confirmAppointment();
+private:
+	Customer& customer;
+	Service& service;
+	Employee& workingEmployee;
+	string appointmentDate;
+	string Employee_name;
+};
+
 
