f346f5742acf094963d17c6ca1484223151f3111
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:23:16 +0300

    Update Engineer.cpp

--- lab2/Repair company/Engineer/Engineer.cpp
+++ lab2/Repair company/Engineer/Engineer.cpp
@@ -1,1 +1,17 @@
-
+#include "Engineer.h"
+#include<iostream>
+#include<string>
+using namespace std;
+Engineer::Engineer(string Employee_name, int Employee_ID) : Employee(Employee_name, Employee_ID) {}
+void Engineer::perform_task() {
+    cout << "Engineer is doing technical job" << endl;
+}
+void Engineer::setCertificationLevel(int level) {
+    certificationLevel = level;
+}
+void Engineer::setSpecialization(string specialization) {
+    this->specialization = specialization;
+}
+void Engineer::performQualityCheck() {
+    cout << "Engineer " << Employee_name << " is performing a quality check" << endl;
+}
