7ad32bf2af385ecfa15b9311f8e05b976c0fe727
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:22:06 +0300

    Update ElectricalInstallationService.cpp

--- lab2/Repair company/ElectricalInstallationService/ElectricalInstallationService.cpp
+++ lab2/Repair company/ElectricalInstallationService/ElectricalInstallationService.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+#include<string>
+#include "ElectricalInstallationService.h"
+using namespace std;
+ElectricInstallationWork::ElectricInstallationWork() : Service("Electric installation", 150.0) {}
+void ElectricInstallationWork::performWiringInspection() {
+    cout << "Conducting wiring inspection for electrical installation" << endl;
+}
+void ElectricInstallationWork::setVoltage(int voltage) {
+    this->voltage = voltage;
+}
+void ElectricInstallationWork::setCircuitCapacity(string capacity) {
+    circuitCapacity = capacity;
+}
