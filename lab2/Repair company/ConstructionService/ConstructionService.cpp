68abf24358949633e1d87300aca505f59783695c
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:20:57 +0300

    Update ConstructionService.cpp

--- lab2/Repair company/ConstructionService/ConstructionService.cpp
+++ lab2/Repair company/ConstructionService/ConstructionService.cpp
@@ -1,1 +1,11 @@
-
+#include<iostream>
+#include<string>
+#include "ConstructionService.h"
+using namespace std;
+ConstructionService::ConstructionService() : Service("Construction", 160.0) {}
+void ConstructionService::performSafetyInspection() {
+    cout << "Performing safety inspection at the construction site" << endl;
+}
+void ConstructionService::setWorkersCount(int count) {
+    workersCount = count;
+}
