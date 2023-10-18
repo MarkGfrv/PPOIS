cbc96a1cbc45c45f31cacc669b8bf11502f73fc8
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:23:38 +0300

    Update Manager.h

--- lab2/Repair company/Manager/Manager.h
+++ lab2/Repair company/Manager/Manager.h
@@ -1,1 +1,15 @@
-
+#pragma once
+#include "Employee.h"
+#include<iostream>
+#include<string>
+using namespace std;
+class Manager: public Employee {
+public:
+	Manager(string Employee_name, int Employee_ID, string subdivision);
+	void perform_task() override;
+	void performMonthlyMeeting();
+	void setProjectsManaged(int projects);
+private:
+	string subdivision;
+	int projectsManaged;
+};
