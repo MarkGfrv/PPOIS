79d7899669357f6babee1c8ff2c7f197a91152d7
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:20:31 +0300

    Update ConstructionService.h

--- lab2/Repair company/ConstructionService/ConstructionService.h
+++ lab2/Repair company/ConstructionService/ConstructionService.h
@@ -1,1 +1,14 @@
+#pragma once
+#include "Service.h"
+#include<iostream>
+#include<string>
+using namespace std;
+class ConstructionService : public Service {
+public:
+	ConstructionService();
+	void performSafetyInspection();
+	void setWorkersCount(int count);
+private:
+	int workersCount;
+};
 
