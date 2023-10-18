03cc1851706f1461a5fdd9c15f1b2aeb56761b61
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:24:27 +0300

    Update PlumbingService.cpp

--- lab2/Repair company/PlumbingService/PlumbingService.cpp
+++ lab2/Repair company/PlumbingService/PlumbingService.cpp
@@ -1,1 +1,8 @@
-
+#include<iostream>
+#include<string>
+#include "PlumbingService.h"
+using namespace std;
+PlumbingService::PlumbingService() : Service("Plumbing", 120.0) {}
+void PlumbingService::inspectPlumbing() {
+    cout << "Inspecting plumbing system before repair" << endl;
+}
