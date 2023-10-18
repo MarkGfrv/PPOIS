8a18ca905edeb2b7b717236e0f62e9f5b154f716
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:25:04 +0300

    Update RequestedService.cpp

--- lab2/Repair company/RequestedService/RequestedService.cpp
+++ lab2/Repair company/RequestedService/RequestedService.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+#include<string>
+#include "RequestedService.h"
+using namespace std;
+RequestedService::RequestedService(Customer& customer, Service& service, Employee& workingEmployee) : customer(customer), service(service), workingEmployee(workingEmployee) {}
+void RequestedService::CompleteOrder() {
+    cout << "Order completed by " << Employee_name << endl;
+}
+void RequestedService::setAppointmentDate(string date) {
+    appointmentDate = date;
+}
+void RequestedService::confirmAppointment() {
+    cout << "Appointment confirmed for " << appointmentDate << endl;
+}
 
