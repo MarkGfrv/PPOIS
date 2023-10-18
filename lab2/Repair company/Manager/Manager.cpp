bade7ec39244b398c295bc11be65ba28a384c516
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:23:55 +0300

    Update Manager.cpp

--- lab2/Repair company/Manager/Manager.cpp
+++ lab2/Repair company/Manager/Manager.cpp
@@ -1,1 +1,14 @@
-
+#include "Manager.h"
+#include<iostream>
+#include<string>
+using namespace std;
+Manager::Manager(string Employee_name, int Employee_ID, string subdivision) : Employee(Employee_name, Employee_ID), subdivision(subdivision) {}
+void Manager::perform_task() {
+    cout << "Manager " << Employee_name << " is working in the " << subdivision << " subdivision" << endl;
+}
+void Manager::performMonthlyMeeting() {
+    cout << "Manager " << Employee_name << " conducts a monthly meeting" << endl;
+}
+void Manager::setProjectsManaged(int projects) {
+    projectsManaged = projects;
+}
