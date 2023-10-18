6ac1669de683bb861c4c9ee486f4c124e969f034
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:22:25 +0300

    Update Employee.h

--- lab2/Repair company/Employee/Employee.h
+++ lab2/Repair company/Employee/Employee.h
@@ -1,1 +1,14 @@
-
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+class Employee {
+public:
+	Employee(string Employee_name, int Employee_ID);
+	void setSalary(double salary);
+	virtual void perform_task() = 0;
+protected:
+	string Employee_name;
+	int Employee_ID;
+	double salary;
+};
