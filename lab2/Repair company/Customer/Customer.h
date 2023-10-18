2ac7be33a93c9fbe92f518af6a1aabb3678317a4
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:21:17 +0300

    Update Customer.h

--- lab2/Repair company/Customer/Customer.h
+++ lab2/Repair company/Customer/Customer.h
@@ -1,1 +1,15 @@
-
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+class Customer {
+public:
+	Customer(string Customer_name, string contactInformation);
+	void placeOrder();
+	void setAddress(string address);
+	string getCustomerName();
+private:
+	string Customer_name;
+	string contactInformation;
+	string address;
+};
