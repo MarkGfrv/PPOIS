87435ecbbe935f4978fe4629952c3291cefe5c2f
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:25:44 +0300

    Update Service.cpp

--- lab2/Repair company/Service/Service.cpp
+++ lab2/Repair company/Service/Service.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+#include<string>
+#include "Service.h"
+using namespace std;
+Service::Service(string ServiceType, double ServiceCost) : ServiceType(ServiceType), ServiceCost(ServiceCost), duration(0), materialsUsed("") {}
+void Service::PerformingService() {
+    cout << "Performing " << ServiceType << " service with the cost " << ServiceCost << endl;
+}
+void Service::setDuration(int duration) {
+    this->duration = duration;
+}
+void Service::setMaterialsUsed(string materials) {
+    materialsUsed = materials;
+}
+string Service::getServiceType() {
+    return ServiceType;
+}
+double Service::getServiceCost() {
+    return ServiceCost;
+}
