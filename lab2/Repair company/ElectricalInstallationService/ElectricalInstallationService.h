2847933a10b575663a1187ed03199b355ae078e6
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:21:52 +0300

    Update ElectricalInstallationService.h

--- lab2/Repair company/ElectricalInstallationService/ElectricalInstallationService.h
+++ lab2/Repair company/ElectricalInstallationService/ElectricalInstallationService.h
@@ -1,1 +1,16 @@
+#pragma once
+#include "Service.h"
+#include<iostream>
+#include<string>
+using namespace std;
+class ElectricInstallationWork : public Service {
+public:
+	ElectricInstallationWork();
+	void performWiringInspection();
+	void setVoltage(int voltage);
+	void setCircuitCapacity(string capacity);
+private:
+	int voltage;
+	string circuitCapacity;
+};
 
