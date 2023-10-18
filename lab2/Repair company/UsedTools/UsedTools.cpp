a9d31283bbe2733aa5804e2c0d0443163f186a11
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:26:21 +0300

    Update UsedTools.cpp

--- lab2/Repair company/UsedTools/UsedTools.cpp
+++ lab2/Repair company/UsedTools/UsedTools.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+#include<string>
+#include "UsedTools.h"
+using namespace std;
+UsedTools::UsedTools(string nameofUsedTool) : nameofUsedTool(nameofUsedTool) {}
 
+void UsedTools::useToolforService() {
+    cout << "Currently using " << nameofUsedTool << " for the repair service" << endl;
+}
+
+void UsedTools::setCondition(string condition) {
+    toolCondition = condition;
+}
