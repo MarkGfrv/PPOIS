40665af29eb462b60db97fcd981c1a11822b701b
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 00:25:31 +0300

    Update Service.h

--- lab2/Repair company/Service/Service.h
+++ lab2/Repair company/Service/Service.h
@@ -1,1 +1,18 @@
-
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+class Service {
+public:
+	Service(string ServiceType, double ServiceCost);
+	void PerformingService();
+	void setDuration(int duration);
+	void setMaterialsUsed(string materials);
+	string getServiceType();
+	double getServiceCost();
+protected:
+	string ServiceType;
+	double ServiceCost;
+	int duration;
+	string materialsUsed;
+};
