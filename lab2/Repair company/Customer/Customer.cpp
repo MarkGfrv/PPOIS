3e5a6d3003274df2f7d5a5ee55537dec8aec2350
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 09:33:34 +0300

    Update Customer.cpp

--- lab2/Repair company/Customer/Customer.cpp
+++ lab2/Repair company/Customer/Customer.cpp
@@ -2,13 +2,13 @@
 #include<string>
 #include "Customer.h"
 using namespace std;
-Customer::Customer(string Customer_name, string contactInformation) : Customer_name(Customer_name), contactInformation(contactInformation) {}
+Customer::Customer(string customerName, string contactInformation) : customerName(customerName), contactInformation(contactInformation) {}
 void Customer::placeOrder() {
-    cout << "Customer " << Customer_name << " has placed an order for repair services." << endl;
+    cout << "Customer " << customerName << " has placed an order for repair services." << endl;
 }
 void Customer::setAddress(string address) {
     this->address = address;
 }
 string Customer::getCustomerName() {
-    return Customer_name;
+    return customerName;
 }
