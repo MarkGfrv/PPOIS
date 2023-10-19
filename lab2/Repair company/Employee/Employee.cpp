f2d7dd25464321c18a613af567ff70fe45e82850
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 09:35:16 +0300

    Update Employee.cpp

--- lab2/Repair company/Employee/Employee.cpp
+++ lab2/Repair company/Employee/Employee.cpp
@@ -2,7 +2,7 @@
 #include<string>
 #include "Employee.h"
 using namespace std;
-Employee::Employee(string Employee_name, int Employee_ID) : Employee_name(Employee_name), Employee_ID(Employee_ID), salary(0.0) {}
+Employee::Employee(string Employee_name, int Employee_ID) : employeeName(employeeName), employeeID(employeeID), salary(0.0) {}
 void Employee::setSalary(double salary) {
     this->salary = salary;
 }
