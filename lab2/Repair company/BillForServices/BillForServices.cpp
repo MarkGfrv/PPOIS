c705114f2bb17144e44bcd737a948603c7771f90
Марк Гафаров <113544592+MarkGfrv@users.noreply.github.com>
2023-10-19 09:31:50 +0300

    Update BillForServices.cpp

--- lab2/Repair company/BillForServices/BillForServices.cpp
+++ lab2/Repair company/BillForServices/BillForServices.cpp
@@ -2,9 +2,9 @@
 #include<string>
 #include "BillForServices.h"
 using namespace std;
-BillForServices::BillForServices(Customer& customer, Service& service, double TotalCost) : customer(customer), service(service), TotalCost(TotalCost) {}
+BillForServices::BillForServices(Customer& customer, Service& service, double totalCost) : customer(customer), service(service), totalCost(totalCost) {}
 void BillForServices::generateBill() {
-    cout << "Total Bill for " << customer.getCustomerName() << ": " << service.getServiceType() << " - " << TotalCost << "$" << endl;
+    cout << "Total Bill for " << customer.getCustomerName() << ": " << service.getServiceType() << " - " << totalCost << "$" << endl;
 }
 void BillForServices::setPaymentStatus(string status) {
     paymentStatus = status;
